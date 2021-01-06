11e7b5e754ed61c708f57b6216f7dc2783f025fb
Istiaq Nirab <istiaqanirab202@gmail.com>
2021-01-06 19:49:12 +0600

    ready for module usage

--- bulk-products-selling.php
+++ bulk-products-selling.php
@@ -1,45 +1,7 @@
 <?php
-/*
-Plugin Name: Bulk Products Selling
-Plugin URI: https://springdevs.com
-Description: Woocommerce missing features in group product
-Version: 1.0.0
-Author: SpringDevs
-Author URI: https://springdevs.com
-License: GPLv2
-License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: sdevs_wea
-Domain Path: /languages
-*/
-
-/**
- * Copyright (c) 2021 SpringDevs (email: contact@spingdevs.com). All rights reserved.
- *
- * Released under the GPL license
- * http://www.opensource.org/licenses/gpl-license.php
- *
- * This is an add-on for WordPress
- * http://wordpress.org/
- *
- * **********************************************************************
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * **********************************************************************
- */
 
 // don't call the file directly
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit;
 }
 
@@ -50,7 +12,8 @@ require_once __DIR__ . '/vendor/autoload.php';
  *
  * @class Sdevs_bpselling The class that holds the entire Sdevs_bpselling plugin
  */
-final class Sdevs_bpselling {
+final class Sdevs_bpselling
+{
     /**
      * Plugin version
      *
@@ -71,13 +34,11 @@ final class Sdevs_bpselling {
      * Sets up all the appropriate hooks and actions
      * within our plugin.
      */
-    private function __construct() {
+    private function __construct()
+    {
         $this->define_constants();
 
-        register_activation_hook( __FILE__, [ $this, 'activate' ] );
-        register_deactivation_hook( __FILE__, [ $this, 'deactivate' ] );
-
-        add_action( 'plugins_loaded', [ $this, 'init_plugin' ] );
+        add_action('plugins_loaded', [$this, 'init_plugin']);
     }
 
     /**
@@ -88,10 +49,11 @@ final class Sdevs_bpselling {
      *
      * @return Sdevs_bpselling|bool
      */
-    public static function init() {
+    public static function init()
+    {
         static $instance = false;
 
-        if ( ! $instance ) {
+        if (!$instance) {
             $instance = new Sdevs_bpselling();
         }
 
@@ -105,9 +67,10 @@ final class Sdevs_bpselling {
      *
      * @return mixed
      */
-    public function __get( $prop ) {
-        if ( array_key_exists( $prop, $this->container ) ) {
-            return $this->container[ $prop ];
+    public function __get($prop)
+    {
+        if (array_key_exists($prop, $this->container)) {
+            return $this->container[$prop];
         }
 
         return $this->{$prop};
@@ -120,8 +83,9 @@ final class Sdevs_bpselling {
      *
      * @return mixed
      */
-    public function __isset( $prop ) {
-        return isset( $this->{$prop} ) || isset( $this->container[ $prop ] );
+    public function __isset($prop)
+    {
+        return isset($this->{$prop}) || isset($this->container[$prop]);
     }
 
     /**
@@ -129,13 +93,14 @@ final class Sdevs_bpselling {
      *
      * @return void
      */
-    public function define_constants() {
-        define( 'BPSELLING_VERSION', self::version );
-        define( 'BPSELLING_FILE', __FILE__ );
-        define( 'BPSELLING_PATH', dirname( BPSELLING_FILE ) );
-        define( 'BPSELLING_INCLUDES', BPSELLING_PATH . '/includes' );
-        define( 'BPSELLING_URL', plugins_url( '', BPSELLING_FILE ) );
-        define( 'BPSELLING_ASSETS', BPSELLING_URL . '/assets' );
+    public function define_constants()
+    {
+        define('BPSELLING_VERSION', self::version);
+        define('BPSELLING_FILE', __FILE__);
+        define('BPSELLING_PATH', dirname(BPSELLING_FILE));
+        define('BPSELLING_INCLUDES', BPSELLING_PATH . '/includes');
+        define('BPSELLING_URL', plugins_url('', BPSELLING_FILE));
+        define('BPSELLING_ASSETS', BPSELLING_URL . '/assets');
     }
 
     /**
@@ -143,28 +108,10 @@ final class Sdevs_bpselling {
      *
      * @return void
      */
-    public function init_plugin() {
+    public function init_plugin()
+    {
         $this->includes();
         $this->init_hooks();
-    }
-
-    /**
-     * Placeholder for activation function
-     *
-     * Nothing being called here yet.
-     */
-    public function activate() {
-        $installer = new Springdevs\BPSelling\Installer();
-        $installer->run();
-    }
-
-    /**
-     * Placeholder for deactivation function
-     *
-     * Nothing being called here yet.
-     */
-    public function deactivate() {
-
     }
 
     /**
@@ -172,16 +119,17 @@ final class Sdevs_bpselling {
      *
      * @return void
      */
-    public function includes() {
-        if ( $this->is_request( 'admin' ) ) {
+    public function includes()
+    {
+        if ($this->is_request('admin')) {
             $this->container['admin'] = new Springdevs\BPSelling\Admin();
         }
 
-        if ( $this->is_request( 'frontend' ) ) {
+        if ($this->is_request('frontend')) {
             $this->container['frontend'] = new Springdevs\BPSelling\Frontend();
         }
 
-        if ( $this->is_request( 'ajax' ) ) {
+        if ($this->is_request('ajax')) {
             // require_once BPSELLING_INCLUDES . '/class-ajax.php';
         }
     }
@@ -191,11 +139,9 @@ final class Sdevs_bpselling {
      *
      * @return void
      */
-    public function init_hooks() {
-        add_action( 'init', [ $this, 'init_classes' ] );
-
-        // Localize our plugin
-        add_action( 'init', [ $this, 'localization_setup' ] );
+    public function init_hooks()
+    {
+        add_action('init', [$this, 'init_classes']);
     }
 
     /**
@@ -203,22 +149,14 @@ final class Sdevs_bpselling {
      *
      * @return void
      */
-    public function init_classes() {
-        if ( $this->is_request( 'ajax' ) ) {
+    public function init_classes()
+    {
+        if ($this->is_request('ajax')) {
             // $this->container['ajax'] =  new Springdevs\BPSelling\Ajax();
         }
 
         $this->container['api']    = new Springdevs\BPSelling\Api();
         $this->container['assets'] = new Springdevs\BPSelling\Assets();
-    }
-
-    /**
-     * Initialize plugin for localization
-     *
-     * @uses load_plugin_textdomain()
-     */
-    public function localization_setup() {
-        load_plugin_textdomain( 'sdevs_wea', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
     }
 
     /**
@@ -228,25 +166,25 @@ final class Sdevs_bpselling {
      *
      * @return bool
      */
-    private function is_request( $type ) {
-        switch ( $type ) {
-            case 'admin' :
+    private function is_request($type)
+    {
+        switch ($type) {
+            case 'admin':
                 return is_admin();
 
-            case 'ajax' :
-                return defined( 'DOING_AJAX' );
+            case 'ajax':
+                return defined('DOING_AJAX');
 
-            case 'rest' :
-                return defined( 'REST_REQUEST' );
+            case 'rest':
+                return defined('REST_REQUEST');
 
-            case 'cron' :
-                return defined( 'DOING_CRON' );
+            case 'cron':
+                return defined('DOING_CRON');
 
-            case 'frontend' :
-                return ( ! is_admin() || defined( 'DOING_AJAX' ) ) && ! defined( 'DOING_CRON' );
+            case 'frontend':
+                return (!is_admin() || defined('DOING_AJAX')) && !defined('DOING_CRON');
         }
     }
-
 } // Sdevs_bpselling
 
 /**
@@ -254,7 +192,8 @@ final class Sdevs_bpselling {
  *
  * @return \Sdevs_bpselling|bool
  */
-function sdevs_bpselling() {
+function sdevs_bpselling()
+{
     return Sdevs_bpselling::init();
 }
 
