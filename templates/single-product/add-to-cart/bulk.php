8506cd59e536eae6c38cfaeadce22cf066b0c061
Istiaq Nirab <istiaqanirab202@gmail.com>
2021-01-11 20:05:15 +0600

    fix some major issues

--- templates/single-product/add-to-cart/bulk.php
+++ templates/single-product/add-to-cart/bulk.php
@@ -9,6 +9,10 @@ if (!defined('ABSPATH')) {
 
 global $product;
 
+if (!$product->is_purchasable()) {
+    return;
+}
+
 do_action('bulk_product_before_add_to_cart_form');
 
 $product_ids = get_post_meta($product->get_id(), '_bpselling_bulk_products', true);
@@ -25,6 +29,7 @@ if (!$product_ids || !is_array($product_ids)) $product_ids = [];
         <?php endforeach; ?>
     </ul>
     <br>
+    <?php do_action("woocommerce_before_add_to_cart_button"); ?>
     <button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
 </form>
 <br>
