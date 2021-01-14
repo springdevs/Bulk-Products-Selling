c24aa1f128e2036984a3b66a8fcefb9767d34b09
ok9xnirab <istiaqanirab202@gmail.com>
2021-01-15 00:11:08 +0600

    add inventory support

--- includes/Admin/views/footer-script.php
+++ includes/Admin/views/footer-script.php
@@ -2,9 +2,14 @@
     jQuery(document).ready(function() {
         //for Price tab
         jQuery('#general_product_data .pricing').addClass('show_if_bulk');
+        jQuery('.inventory_options').addClass('show_if_custom').show();
+        jQuery('#inventory_product_data ._sold_individually_field').parent().addClass('show_if_bulk').show();
+        jQuery('#inventory_product_data ._sold_individually_field').addClass('show_if_bulk').show();
 
         <?php if ($is_bulk) { ?>
             jQuery('#general_product_data .pricing').show();
+            jQuery('#inventory_product_data ._sold_individually_field').parent().addClass('show_if_bulk').show();
+            jQuery('#inventory_product_data ._sold_individually_field').addClass('show_if_bulk').show();
         <?php } ?>
     });
 </script>
